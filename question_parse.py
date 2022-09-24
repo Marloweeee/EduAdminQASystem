e93da3b7ee471809f989d57ad09a3f32e10cee23
Marloweeee <convfactor@bupt.edu.cn>
2022-09-24 20:18:41 +0800

    question parser first draft

--- question_parse.py
+++ question_parse.py
@@ -18,8 +18,10 @@ class QuestionPaser:
 
         if entity_key in ['开题','中期','答辩','毕业']:
             cql='MATCH (n:`论文答辩`) WHERE n.name="{}" RETURN {}'.format(entity_key,"n."+", n.".join(entity_dict))
-        if entity_key in ['暑期学校','选课','课程']:
+        if entity_key in ['暑期学校','选课','上课']:
             cql='MATCH (n:`教学服务`) WHERE n.name="{}" RETURN {}'.format(entity_key,"n."+", n.".join(entity_dict))
+        if entity_key in ['国家奖学金','学业奖学金']:
+            cql='MATCH (n:`先进个人`) WHERE n.name="{}" RETURN {}'.format(entity_key,"n."+", n.".join(entity_dict))
 
         return cql
 
@@ -28,8 +30,6 @@ class QuestionPaser:
 
 
 if __name__ == '__main__':
-    ques="可以不参加暑期学校吗,暑期学校的成绩怎么认定"
-    e,q,k=QuestionClassifier().classify(ques)
-    QuestionPaser().parser_main(e,q,k)
+    pass
 
 
